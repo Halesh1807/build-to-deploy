3efe2dae1b1512eca20b542eee392195cdca5297
Halesh1807 <110560517+Halesh1807@users.noreply.github.com>
2022-08-20 11:25:52 +0530

    Add files via upload

---
+++ Jenkinsfile.php
@@ -0,0 +1,23 @@
+pipeline{
+    agent any
+
+    stages{
+        stage ('Build'){
+            step{
+                sh 'mvn clean packege'
+            }
+            post{
+                success{
+                    echo "Archiving the Artifacts"
+                    archiveartifacts Artifacts: '**/target/*.war'
+                }
+            }
+        }
+        stage ('Deploy to tomcat server'){
+            steps{
+                deploy adapters: [tomcat9(path: '', url: 'http://3.109.207.104:8080/')], contextPath: null, war: '**/*.war'
+            }            
+        }
+    }
+
+}
